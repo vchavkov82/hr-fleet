druidoo-dev/fleet

[ADD] fleet_vehicle_history_date_end: New module to assign end date to vehicle's drivers logs.

fleet_vehicle_history_date_end/__manifest__.py
@@ -1,0 +1,18 @@
+# Copyright 2020-Present Druidoo - Manuel Marquez <manuel.marquez@druidoo.io>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Assign date end in vehicle history",
+    "summary": "Automatically assign date end in vehicle "
+    "history when a new driver is assigned.",
+    "category": "Human Resources/Fleet",
+    "author": "Druidoo, Odoo Community Association (OCA)",
+    "maintainers": ["mamcode", "ivantodorovich"],
+    "development_status": "Beta",
+    "website": "https://www.druidoo.io/",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "depends": ["fleet"],
+    "data": [],
+    "installable": True,
+}

vchavkov82/hr-fleet

fleet_vehicle_history_date_end/__manifest__.py
@@ -1,0 +1,18 @@
+# Copyright 2020-Present Druidoo - Manuel Marquez <manuel.marquez@druidoo.io>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Assign date end in vehicle history",
+    "summary": "Automatically assign date end in vehicle "
+    "history when a new driver is assigned.",
+    "category": "Human Resources/Fleet",
+    "author": "Druidoo, Odoo Community Association (OCA)",
+    "maintainers": ["mamcode", "ivantodorovich"],
+    "development_status": "Stable",
+    "website": "https://github.com/OCA/fleet",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "depends": ["fleet"],
+    "data": [],
+    "installable": True,
+}