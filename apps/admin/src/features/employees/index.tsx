0d753eee5e38b5b081d910d56c9ffc9ddda7e4bc
vchavkov82 <vchavkov82@gmail.com>
2026-03-20 21:47:17 +0000

    auto: sync 1 file [2026-03-20 21:47:17]

--- apps/admin/src/features/employees/index.tsx
+++ apps/admin/src/features/employees/index.tsx
@@ -160,7 +160,7 @@ export const EmployeeList: React.FC = () => {
             </Table>
             <TablePagination
               component="div"
-              count={data?.meta.total ?? 0}
+              count={data?.meta?.total ?? 0}
               page={page}
               onPageChange={(_, newPage) => setPage(newPage)}
               rowsPerPage={rowsPerPage}
