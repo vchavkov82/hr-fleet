578fdec42151a73c9d1a9bb226dd97c87234b121
vchavkov82 <vchavkov82@gmail.com>
2026-03-20 19:13:10 +0000

    auto: sync 1 file [2026-03-20 19:13:10]

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -12,7 +12,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   server: { host: true, port: 5011 },
-  site: 'https://docs.hr.assistance.bg',
+  site: 'https://docs.hr.svc.assistance.bg',
 
   integrations: [
     starlight({
