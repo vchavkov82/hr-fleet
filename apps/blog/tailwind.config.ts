9b58fadb542c85feaec1dff11457a9fe58a367a6
vchavkov82 <vchavkov82@gmail.com>
2026-03-21 16:33:38 +0000

    auto: sync 1 file [2026-03-21 16:33:38]

---
+++ apps/blog/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+import { designTokens } from '@hr/design-system/tailwind'
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './content/**/*.{md,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      ...designTokens,
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
+
+export default config
