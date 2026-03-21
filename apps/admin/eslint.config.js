6374ad5e9eab23b25640e0156ecf4043aa82449c
vchavkov82 <vchavkov82@gmail.com>
2026-03-21 09:17:09 +0000

    auto: sync 1 file [2026-03-21 09:17:09]

--- apps/admin/eslint.config.js
+++ apps/admin/eslint.config.js
@@ -1,23 +1,6 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import globals from 'globals'
+import reactConfig from "@hr/eslint-config/react";
 
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2022,
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-    },
-  }
-)
+export default [
+  ...reactConfig,
+  { ignores: ["dist/**"] },
+];
